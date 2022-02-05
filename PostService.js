8bdaa286065b603808316c73f9ff7a18b79f1e79
alinurlibekov <alinurlibekov@gmail.com>
2022-02-05 10:51:05 +0300

    created

---
+++ PostService.js
@@ -0,0 +1,40 @@
+import Post from "./Post.js";
+
+class PostService {
+  async create(post) {
+    const createdPost = await Post.create(post);
+    return res.status(200).json(createdPost);
+  }
+
+  async getAll() {
+    const posts = await Post.find();
+    return posts;
+  }
+  async getOne(id) {
+    if (!id) {
+      throw new Error("Id не указан!");
+    }
+    const post = await Post.findById(id);
+    return post;
+  }
+
+  async update(post) {
+    if (!post._id) {
+      throw new Error("Id не указан!");
+    }
+    const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
+      new: true,
+    });
+    return updatedPost;
+  }
+
+  async delete(id) {
+      if (!id) {
+        throw new Error("Id не указан!");
+      }
+      const post = await Post.findByIdAndDelete(id);
+      return post;
+  }
+}
+
+export default new PostService();
